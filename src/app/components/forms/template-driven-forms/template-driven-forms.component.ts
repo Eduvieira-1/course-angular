8716c1a67353db96af045f3907d2e1428a1f4db8
Eduvieira-1 <edu.vieira1@icloud.com>
2024-01-10 07:24:03 -0300

    template driven

--- src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
 
+  public listComidas = signal<Array<{comida: string, preco: string}>>([
+    {
+      comida: 'x-salada',
+      preco: 'R$ 10'
+    },
+    {
+      comida: 'x-bacon',
+      preco: 'R$ 11'
+    },
+    {
+      comida: 'Coxinha',
+      preco: 'R$ 6'
+    },
+  ]);
+
+  public submitForm(form: NgForm){
+    console.log(form.valid);
+    if(form.valid){
+      console.log(form.value);
+    }
+  }
 }
