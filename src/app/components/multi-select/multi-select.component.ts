2c8ae93d9832152cbfd1cdacaf1a88cdbe3d12ff
Eduvieira-1 <edu.vieira1@icloud.com>
2024-02-02 20:02:09 -0300

    multi-select

---
+++ src/app/components/multi-select/multi-select.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './multi-select.component.html',
+  styleUrl: './multi-select.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MultiSelectComponent {
+
+}
